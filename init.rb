f1d5b5b9da5b42376f0a835d18c5d89f1dac6a0d
Eugene Sypachev <eugene.sypachev@gmail.com>
2011-10-26 11:24:52 +0400

    Update init.rb

--- init.rb
+++ init.rb
@@ -10,8 +10,10 @@ require 'hooks/controller_issues_bulk_edit_before_save_hook'
 Redmine::Plugin.register :redmine_custom_field_extender do
   name 'Redmine Custom Field Extender plugin'
   author 'Eugene Sypachev'
-  description 'This is a plugin for Redmine'
-  version '0.1'
+  description 'Plugin for Redmine that helps you to create new types of custom fields'
+  version '0.1.1'
+  url 'https://github.com/Axblade/redmine_custom_field_extender'
+  author_url 'http://www.redmine.org/users/35480'
   
 
   # extend CustomFieldFormat map
